8817469da98f09865e7ce3b47fc7e2877d8d30b0
Dalitso Mandala <dalitso.mandala.prince@gmail.com>
2024-01-20 20:06:33 +0200

    added livewire

---
+++ app/Livewire/AddTasks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AddTasks extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-tasks');
+    }
+}
